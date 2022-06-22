fc719632780f1b5d36f4b2efb3c986773bd8860c
Akinobu Shigeishi <44592900+heavy-stone@users.noreply.github.com>
2022-06-22 20:37:04 +0900

    [command]flutter pub run flutter_flavorizr

---
+++ lib/flavors.dart
@@ -0,0 +1,25 @@
+enum Flavor {
+  LOCAL,
+  DEV,
+  PROD,
+}
+
+class F {
+  static Flavor? appFlavor;
+
+  static String get name => appFlavor?.name ?? '';
+
+  static String get title {
+    switch (appFlavor) {
+      case Flavor.LOCAL:
+        return 'アプリ名（local）';
+      case Flavor.DEV:
+        return 'アプリ名（dev）';
+      case Flavor.PROD:
+        return 'アプリ名';
+      default:
+        return 'title';
+    }
+  }
+
+}
